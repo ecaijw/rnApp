9f794afe858c4107ce717ae17637e5df0e570133
luke <luke@d7d.com.au>
2024-08-13 12:12:21 +1000

    add an image and background

--- mapBoxInWebViewComponent.tsx
+++ mapBoxInWebViewComponent.tsx
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Button,
   Modal,
+  Image,
 } from "react-native";
 import { WebView } from 'react-native-webview';
 import { htmlContentForMapBoxInWebView } from "./globeMapContent";
@@ -19,10 +20,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
+  buttonImage: {
+    width: 30,  
+    height: 30,  // Set the height of the image
+    marginRight: 10,  // Space between image and button
+  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+    alignItems: 'center',  // Center content vertically
     marginVertical: 10,
+    backgroundColor: '#56A192',  // Set background color
+    padding: 10,  // Add padding
+    borderRadius: 8,  // Add rounded corners
   },
 });
 
@@ -42,6 +52,10 @@ export const MapBoxInWebViewComponent: React.FC<MapBoxInWebViewComponentProps> =
     <Modal visible={isVisible} animationType="slide" onRequestClose={onClose}>
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
+        <Image
+            src = "https://d220yz93drhwe7.cloudfront.net/smp/general/bzai/gsp-app-icon.png"  // Replace with your image URL
+            style={styles.buttonImage}
+          />
           <Button title="Close WebView" onPress={onClose} />
           <Button title="Fly to BZAI" onPress={handleFlyToBZAI} />
         </View>
