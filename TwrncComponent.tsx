644faa0abc53ab2566746b85d8da9eff549aeb0d
luke <luke@d7d.com.au>
2024-08-29 12:47:58 +1000

    responsive design

--- TwrncComponent.tsx
+++ TwrncComponent.tsx
@@ -2,8 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import { Dimensions, View, StyleSheet, Pressable } from "react-native";
-import { Text, TouchableOpacity, Alert } from 'react-native';
-import { Button as MyButton, Image } from 'react-native';
+import { Text, TouchableOpacity, Alert, useWindowDimensions } from 'react-native';
+import { Image } from 'react-native';
 
 import { create } from 'twrnc';
 import customTwrncConfig from './twrnc.config.js';
@@ -31,23 +31,50 @@ const styles = StyleSheet.create({
 });
 
 export const TwrncComponent = () => {
+  const { width, height } = useWindowDimensions();
+  const isPortrait = height >= width;
+
+  const isSmallerThanSm = width < 640;
+  const isSm = width >= 640 && width < 768;
+  const isMd = width >= 768 && width < 1024; // md corresponds to min-width: 768px and max-width: 1023px
+  const isLg = width >= 1024 && width < 1280; // lg corresponds to min-width: 1024px and max-width: 1279px
+  const isXl = width >= 1280 && width < 1536;
+  const is2Xl = width >= 1536;
+  console.log(`screen width: ${width}, ${isSmallerThanSm}, ${isSm}, ${isMd}, ${isLg}, ${isXl}, ${is2Xl}`);
+  console.log(`screen width isSmallerThanSm: ${isSm}`);
+  console.log(`screen width md: ${isMd}`);
+
 
   return (
-    <View style={tailwind`h-75 w-full bg-[#f5fcff] justify-center items-center`}>
-      <Button />
-      <View style={tailwind`p-6 max-w-xs bg-white rounded-lg shadow-lg flex-row items-center`}>
-        <Image
-          style={tailwind`w-12 h-12`}
-          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} // Replace with your actual image source
-        />
-        <View style={tailwind`ml-4`}>
-        <Text style={[tailwind`text-xl font-bold`, { color: '#3b82f6' }]}>fallback color</Text>
-        <Text style={tailwind`text-xl font-bold text-lukeBlue`}>text-lukeBlue</Text>
-        <Text style={tailwind`text-xl font-bold text-luke-500`}>text-luke</Text>
-        <Text style={tailwind`text-xl font-bold text-rose-600`}>ChitChat</Text>
-          <Text style={tailwind`text-slate-500`}>You have a new message!</Text>
+    <View style={tailwind`h-full w-full bg-[#f5fcff] justify-center items-center`}>
+      <View style={tailwind`max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-visible md:max-w-4xl mb-[20px]`}>
+        <View style={tailwind`flex ${isPortrait ? 'flex-col' : 'flex-row'}`}>
+          {/* Adjusting the Image View to handle both portrait and landscape correctly */}
+          <View style={tailwind`md:shrink-0 ${isPortrait ? 'mb-4' : 'mr-4'} items-center justify-center`}>
+            <Image
+              style={tailwind`h-16 w-48 ml-5`}
+              source={{ uri: 'https://d220yz93drhwe7.cloudfront.net/smp/general/bzai/bzai-logo.png' }}
+              resizeMode="contain"
+              alt="Modern building architecture"
+            />
+          </View>
+          {/* Text Content */}
+          <View style={tailwind`p-2 ${isXl ? 'hidden' : ''}`}>
+            <Text style={tailwind`uppercase tracking-wide text-sm ${isPortrait ? 'text-indigo-500' : 'text-lukeBlue'} font-semibold`}>
+              Company retreats
+            </Text>
+            <TouchableOpacity style={tailwind`mt-1`} onPress={() => {}}>
+              <Text style={tailwind`text-lg leading-tight font-medium text-black hover:underline`}>
+                Incredible accommodation for your team
+              </Text>
+            </TouchableOpacity>
+            <Text style={tailwind`mt-2 text-slate-500`}>
+              Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
+            </Text>
+          </View>
         </View>
       </View>
+      <Button />
     </View>
   );
 };
